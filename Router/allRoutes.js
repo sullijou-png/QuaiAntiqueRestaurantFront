3aa3402134d47eaf25a87254c09577c68f52bf78
Sullivan Jousset <sullijou@gmail.com>
2026-02-28 10:28:17 +0100

    Sign in / Sign up page

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,9 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/galerie", "Galerie", "/pages/galerie.html")
+    new Route("/galerie", "Galerie", "/pages/galerie.html"),
+    new Route("/signin", "Sign In", "/pages/signin.html"),
+    new Route("/signup","Inscription", "/pages/signup.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
