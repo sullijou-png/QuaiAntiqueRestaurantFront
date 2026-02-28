e39062ea11c9ad6c4ef5f50f6ecc780222713718
Sullivan Jousset <sullijou@gmail.com>
2026-02-28 10:53:47 +0100

    Ajout Authentification

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,8 +4,10 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "Galerie", "/pages/galerie.html"),
-    new Route("/signin", "Sign In", "/pages/signin.html"),
-    new Route("/signup","Inscription", "/pages/signup.html"),
+    new Route("/signin", "Sign In", "/pages/auth/signin.html"),
+    new Route("/signup","Inscription", "/pages/auth/signup.html"),
+    new Route("/account", "Mon Compte", "/pages/auth/account.html"),
+    new Route("/editPassword", "Modifier mon mot de passe", "/pages/auth/editPassword.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
